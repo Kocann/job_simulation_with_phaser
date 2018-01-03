de2916e770c978b036caf21df99c87c29ca7ec83
Kocann <kocela.ann@gmail.com>
2018-01-03 19:20:20 +0100

    Initial setup

---
+++ Src/Index.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+import './../node_modules/phaser/build/custom/pixi';
+import './../node_modules/phaser/build/custom/p2.js';
+import './../node_modules/phaser/build/custom/phaser-split';
+import 'phaser';
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+var phaserModule = path.join(__dirname, '/node_modules/phaser/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js'),
+    pixi = path.join(phaserModule, 'build/custom/pixi.js'),
+    p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "sass-loader" // compiles Sass to CSS
+      }]
+    },
+    { test: /pixi.js/, 
+        use: [{
+            loader: "script-loader"}]
+    },
+    { test: /p2.js/, 
+        use: [{
+            loader: "script-loader"}]
+    }]
+},
+resolve: {
+    alias: {
+        'phaser': phaser,
+        'pixi.js': pixi,
+        'p2': p2
+    }
+}
+};
